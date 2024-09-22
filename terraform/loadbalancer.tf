b923d755e2ee41607aafdf13de29287713753903
Benjamin <149324390+astine85@users.noreply.github.com>
2024-09-22 07:59:39 +0100

    Update loadbalancer.tf

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_target_group" "main" {
-  name        = "marketvector-tg"
+  name        = "first-real-project-tg"
   port        = var.container_port
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
