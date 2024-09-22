57bde0cbf9f70704763f86f5b0fff422d8cf97ca
Benjamin <149324390+astine85@users.noreply.github.com>
2024-09-22 07:52:27 +0100

    Update cloudwatch.tf

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
-  name              = "/ecs/marketvector-app"        
+  name              = "/ecs/first-real-project-app"        
   retention_in_days = 30
 }
